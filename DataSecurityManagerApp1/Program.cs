873963c99417b0f600e0d2d0670805fe970fc14e
unknown <232909072092-21>
2026-03-12 11:13:33 +0300

    Добавила дизайн формы

--- DataSecurityManagerApp1/Program.cs
+++ DataSecurityManagerApp1/Program.cs
@@ -10,8 +10,10 @@ namespace DataSecurityManagerApp1
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DataSecurityForm());
         }
     }
 }
